ea2166fe795164a706d331f7f2869db72ae41434
Jan Uerpmann <januerpmann@mac-pro.fritz.box>
2013-12-13 09:20:58 +0100

    Empty DragonTable added

---
+++ Sardinia 1865/DragonTable.h
@@ -0,0 +1,23 @@
+//
+//  DragonTable.h
+//  Sardinia 1865
+//
+//  Created by Jan Uerpmann on 13/12/13.
+//  Copyright (c) 2013 Jan Uerpmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DragonTable : NSObject
+
+@end
+
+// 15 x 6
+
+//Border Green -> white (1st white)
+//4
+//6
+//8
+//10
+//12
+//14
---
+++ Sardinia 1865/DragonTable.m
@@ -0,0 +1,13 @@
+//
+//  DragonTable.m
+//  Sardinia 1865
+//
+//  Created by Jan Uerpmann on 13/12/13.
+//  Copyright (c) 2013 Jan Uerpmann. All rights reserved.
+//
+
+#import "DragonTable.h"
+
+@implementation DragonTable
+
+@end
